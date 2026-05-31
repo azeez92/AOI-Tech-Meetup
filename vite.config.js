ff26a7cc107ca3b6dabbd9ef4d94b2fd0da64579
azeez92 <asodiq@oceanichealthng.com>
2026-05-31 13:59:16 +0100

    sens

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+   base: "/AOI-Tech-Meetup/",
   plugins: [react()],
-  base: "/AOI-Tech-Meetup/"
+ 
 })
